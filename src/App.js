29e860e156f59e982c540fbf6a555fbfde61efc3
Alexander784 <alexandernyaga1999@gmail.com>
2024-04-15 22:15:57 +0300

    Implement different component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import Home from "./pages/home/Home";
+import Products from "./pages/products/Products";
+import Users from "./pages/users/Users";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/products" element={<Products />}/>
+          <Route path="/users" element={<Users />}/>
+
+
+        </Routes>
+      </Router>
       
     </div>
   );
