3b89e84038c25f2bcfdda5605190b42739e24518
Alexander784 <alexandernyaga1999@gmail.com>
2024-04-16 09:34:16 +0300

    Implement Footer component

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import "./footer.scss";
 
 const Footer = () => {
   return (
-    <div className='footer'>Footer</div>
+    <div className='footer'>
+      <span>Alexander</span>
+      <span>@Lexander Admin Dashboard</span>
+      </div>
   )
 }
 
