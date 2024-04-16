3e1cfa3e03d419966234e4bcdbbaeeec9cc10452
Alexander784 <alexandernyaga1999@gmail.com>
2024-04-16 10:51:18 +0300

    Customize menu component

--- src/menu/Menu.jsx
+++ src/menu/Menu.jsx
@@ -1,34 +1,24 @@
 import { Link } from 'react-router-dom';
+import "./menu.scss";
+import { menu } from "../../src/data";
+
 
 const Menu = () => {
   return (
     <div className='menu'>
-      <div className='item'>
-        <span className="title">MAIN</span>
-        <Link to="/">
-        <img src="" alt="" />
-        <span className="listItemTitle">Home</span>
+      {menu.map ((item) => (
+      <div className='item' key={item.id}>
+        <span className="title">{item.title}</span>
+         {item.listItems.map((listItem) => (
+          <Link to="/" className='listItem' key={listItem.id}>
+        <img src={listItem.icon} alt="" />
+        <span className="listItemTitle">{listItem.title}</span>
         </Link>
-
-        <Link to="/">
-        <img src="/profile.svg" alt="" />
-        <span className="listItemTitle">Profile</span>
-        </Link>
+         ))}
       </div>
-
-      <div className='item'>
-        <span className="title">MAIN</span>
-        <Link to="/">
-        <img src="" alt="" />
-        <span className="listItemTitle">Home</span>
-        </Link>
-
-        <Link to="/">
-        <img src="/profile.svg" alt="" />
-        <span className="listItemTitle">Profile</span>
-        </Link>
-      </div>
-    </div>
+       ))}
+    </div> 
+     
   )
 }
 
