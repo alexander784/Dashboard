f8e473350a252fd1ec6af1976a619ae0dc254244
Alexander784 <alexandernyaga1999@gmail.com>
2024-04-16 07:54:35 +0300

    Homepage implementation

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import NavBar from "./components/navbar/NavBar";
+import Footer from "./components/navbar/footer/Footer";
+import Menu from "./components/navbar/footer/menu/Menu";
 import Home from "./pages/home/Home";
 import Products from "./pages/products/Products";
 import Users from "./pages/users/Users";
@@ -11,6 +14,17 @@ import {
 const Layout = () => {
   return (
   <div className="main">
+    <NavBar />
+    <div className="container">
+
+      <div className="menuContainer">
+        <Menu />
+      </div>
+      <div className="contentContainer">
+
+      </div>
+    </div>
+    <Footer />
 
   </div>
   )
@@ -18,9 +32,10 @@ const Layout = () => {
 function App() {
   return (
     <div className="App">
-      {/* <Home /> */}
+      <Home />
       <Router>
         <Routes>
+        <Route path="/" element={<Layout />}/>
           <Route path="/" element={<Home />}/>
           <Route path="/products" element={<Products />}/>
           <Route path="/users" element={<Users />}/>
