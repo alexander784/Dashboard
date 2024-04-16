4c29bab8e59599cd80901802ecef60415f13070d
Alexander784 <alexandernyaga1999@gmail.com>
2024-04-16 13:33:58 +0300

    Implement TopBox component

--- src/components/topbox/TopBox.jsx
+++ src/components/topbox/TopBox.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { topDealUsers } from '../../data';
 
 const TopBox = () => {
   return (
     <div className='topBox'>
         <h1>Top Deals</h1>
+        <div className='list'>
+          {topDealUsers.map((user) => (
+            <div className='listitem' 
+            key={user.id}>
+              <div className="user">
+                <img src="" alt="" />
+                <div className="userTexts">
+                  <span className='username'>{user.username}</span>
+                  <span className='email'>{user.email}</span>
+
+                </div>
+              </div>
+
+            </div>
+          ))}
+        </div>
     </div>
   )
 }
