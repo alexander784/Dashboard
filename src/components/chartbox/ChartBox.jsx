75f13225f9d54bd40d4be59a3c14bab20e2ba188
Alexander784 <alexandernyaga1999@gmail.com>
2024-04-16 15:26:52 +0300

    ChartBox

--- src/components/chartbox/ChartBox.jsx
+++ src/components/chartbox/ChartBox.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import "./chartbox.scss"
+import { Link } from 'react-router-dom';
+
 
 const ChartBox = () => {
   return (
-    <div>ChartBox</div>
-  )
+    <div className='chartBox'>
+        <div className='boxInfo'></div>
+        <div className='chartInfo'></div>
+        <div className="title">
+            <img src="/user.svg" alt="" />
+            <span>Total Users</span>
+        </div>
+        <h1>11.238</h1>
+        <Link to="/">View All</Link>
+        <div className='chartInfo'>
+            <div className="chart"></div>
+            <div className="texts">
+                <span className='percentage'>45%</span>
+                <span className='duration'>this month</span>
+
+            </div>
+
+        </div>
+    </div>
+  );
 }
 
 export default ChartBox;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import TopBox from "../../components/topbox/TopBox";
 import "./home.scss";
+import ChartBox from "../../components/chartbox/ChartBox";
 
 
 const Home = () => {
@@ -8,10 +9,18 @@ const Home = () => {
       <div className="box box1">
         <TopBox />
       </div>
-      <div className="box box2">ChartBox</div>
-      <div className="box box2">ChartBox</div>
-      <div className="box box2">ChartBox</div>
-      <div className="box box2">ChartBox</div>
+      <div className="box box2">ChartBox
+      <ChartBox />
+      </div>
+      <div className="box box2">ChartBox
+      <ChartBox />
+      </div>
+      <div className="box box2">ChartBox
+      <ChartBox />
+      </div>
+      <div className="box box2">ChartBox
+      <ChartBox />
+      </div>
       <div className="box box6">Box6</div>
       <div className="box box7">Box7</div>
       <div className="box box8">Box8</div>
