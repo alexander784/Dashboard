f4dbfc9bb72d978d6fe59c13f46395fc2acdb2d1
Alexander784 <alexandernyaga1999@gmail.com>
2024-04-26 19:57:41 +0300

    pass props responsible for chartbox data

--- src/components/chartbox/ChartBox.jsx
+++ src/components/chartbox/ChartBox.jsx
@@ -4,73 +4,41 @@ import { Link } from 'react-router-dom';
 import { LineChart, Line, ResponsiveContainer, Tooltip } from 'recharts';
 
 
-const data = [
-    {
-      name: 'Page A',
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: 'Page B',
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: 'Page C',
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: 'Page D',
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: 'Page E',
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'Page F',
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: 'Page G',
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-  ];
 
-const ChartBox = () => {
+type Props = {
+    color:string;
+    icon:string
+    title:string
+    dataKey:string
+    number:number | String
+    percentage:number
+    chartbox:object
+
+
+}
+const ChartBox = (props) => {
   return (
     <div className='chartBox'>
         <div className='boxInfo'></div>
         <div className='chartInfo'></div>
         <div className="title">
-            <img src="/user.svg" alt="" />
-            <span>Total Users</span>
+            <img src={props.icon} alt="" />
+            <span>{props.title}</span>
         </div>
-        <h1>11.238</h1>
+        <h1>{props.number}</h1>
         <Link to="/">View All</Link>
         <div className='chartInfo'>
             <div className="chart">
                 {/* Chart container */}
 
-                <ResponsiveContainer width="100%" height="100%">
-        <LineChart width={300} height={100} data={data}>
+                <ResponsiveContainer width="99%" height="100%">
+        <LineChart data={props.ChartData}>
             <Tooltip 
             contentStyle={{background:"transparent", border:"none"}}
             labelStyle={{display:"none"}} 
             position={{x: 10, y:50}}/>
-          <Line type="monotone" dataKey="pv" stroke="#8884d8" strokeWidth={2}
+          <Line type="monotone" dataKey={props.datakey}
+           stroke={props.color} strokeWidth={2}
           dot={false} />
         </LineChart>
       </ResponsiveContainer>
@@ -78,7 +46,8 @@ const ChartBox = () => {
                 
             </div>
             <div className="texts">
-                <span className='percentage'>45%</span>
+                <span className='percentage' style={{color:props.percentage <0 ? "tomato" : "limegreen"}}>
+                    {props.percentage}</span>
                 <span className='duration'>this month</span>
 
             </div>
